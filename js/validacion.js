ac3bd4cf27a2e36e447b086b743ed99cca0a69d3
Paula Caceres <paucaceresgonph@gmail.com>
2024-08-15 15:06:51 -0300

    Contraseñas_iguales

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,7 @@
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const regBtn = document.getElementById("regBtn")
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +9,14 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function validatePasswords() {
+    return password1.value === password2.value;
+  }
+    regBtn.addEventListener("click", () => {
+    if (!validatePasswords()) {
+      return;
+    }
+
+    console.log("Enviando el formulario...");
+});
