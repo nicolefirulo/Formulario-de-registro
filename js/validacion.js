4c03f3c2ec2612cb5e58e43a01d6633b361be4fa
Nicole07v <nicolefirulo02@gmail.com>
2024-08-15 14:22:31 -0300

    validacion-passwords

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,11 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function password() {
+    if(password1.length < 6 || password2.length < 6){
+      return showAlertError()
+     } else {
+       return showAlertSuccess()
+     }
+}
